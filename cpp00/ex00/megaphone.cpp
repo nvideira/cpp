e3f3a7fda106d15c6cc60ee4074a351e59f65237
Nuno Videira <nvideira@c2r8s6.42lisboa.com>
2023-08-29 17:57:42 +0100

    ex00 done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,14 +6,31 @@
 /*   By: nvideira <nvideira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 23:13:35 by nvideira          #+#    #+#             */
-/*   Updated: 2023/08/24 02:07:11 by nvideira         ###   ########.fr       */
+/*   Updated: 2023/08/29 17:55:50 by nvideira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int	main(int ac, string av)
+int	main(int ac, char **av)
 {
-	std::cout << av;
+	if(ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	int	j = 1;
+	while (j < ac)
+	{
+		int	i = 0;
+		while(av[j][i])
+		{
+			std::cout << (char )toupper(av[j][i]);
+			i++;
+		}
+		j++;
+	}
+	std::cout << std::endl;
 	return 0;
 }
